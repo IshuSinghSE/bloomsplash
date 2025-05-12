98496d2f3dc2971d4fd45423eb0b99239a0a0dee
IshuSinghSE <ishu.111636@gmail.com>
2025-05-13 00:11:44 +0530

    feat: added github action workflow for build and release apks

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -54,10 +54,10 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            // proguardFiles(
+            //     getDefaultProguardFile("proguard-android-optimize.txt"),
+            //     "proguard-rules.pro"
+            // )
         }
     }
 }
