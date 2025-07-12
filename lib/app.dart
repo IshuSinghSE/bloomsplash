80f90bdc913997fd8a9ffa5054593ff85c150d28
IshuSinghSE <ishu.111636@gmail.com>
2025-07-13 01:33:11 +0530

    bug: guest user, fix to not avoid the logging screen

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'app/providers/favorites_provider.dart';
 import 'app/providers/auth_provider.dart';
 import 'features/home/screens/home_page.dart';
+import 'features/welcome/screens/welcome_page.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 class App extends StatelessWidget {
@@ -30,10 +31,27 @@ class App extends StatelessWidget {
       child: MaterialApp(
         title: 'BloomSplash',
         theme: ThemeData.dark(),
-        home: HomePage(preferencesBox: preferencesBox),
         debugShowCheckedModeBanner: false,
         navigatorObservers: [observer],
+        home: AuthGate(preferencesBox: preferencesBox),
       ),
     );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  final Box preferencesBox;
+  const AuthGate({super.key, required this.preferencesBox});
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    // If AuthProvider exposes a loading state, use it. Otherwise, assume false means not logged in.
+    // If isLoggedIn is always non-nullable, remove the null check.
+    if (authProvider.isLoggedIn == false) {
+      return WelcomePage(preferencesBox: preferencesBox);
+    }
+    // User is logged in
+    return HomePage(preferencesBox: preferencesBox);
   }
 }
